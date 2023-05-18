52cada8a5733d1764b96b3750f1debe61ae3052d
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 15:52:39 +0530

    Getting data to Profile

--- todo-react/src/Profile.js
+++ todo-react/src/Profile.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { account } from './appwrite-config'
 
 function Profile() {
+    const [userdetails,setUserdetails] = useState()
+    useEffect(()=>{
+        account.get().then((res)=>{
+            setUserdetails(res)
+        }).catch((err)=>{
+            console.log("Error")
+        }) 
+    },[])
   return (
-    <div>Profile</div>
+    <></>
   )
 }
 
