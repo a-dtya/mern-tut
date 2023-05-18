85af15b234acef60e7b00bc46b4908379a223c7c
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 09:35:19 +0530

    configured appwrite

--- todo-react/src/appwrite.js
+++ todo-react/src/appwrite.js
@@ -2,3 +2,7 @@ import {Client,Account,Databases} from 'appwrite'
 
 const client = new Client()
 client.setEndpoint("https://cloud.appwrite.io/v1").setProject("6465a18b55f531cc84a8")
+
+export const account = new Account(client)
+export const databases = new Databases(client,"6465a1e5c4ed1e82b0f5")
+
