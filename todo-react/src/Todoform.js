4f712c01db11e96676338889c7e719451a7db8a0
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 19:43:43 +0530

    Todoform UI

--- todo-react/src/Todoform.js
+++ todo-react/src/Todoform.js
@@ -3,7 +3,7 @@ import { databases } from './appwrite-config'
 import {v4 as uuidv4} from 'uuid'
 
 function Todoform() {
-    const [todo,seTodo] = useState("")
+    const [todo,setTodo] = useState("")
     const handleSubmit = ()=>{databases.createDocument("6465a1f120228e9beaf7",uuidv4(),{
         todo:todo
     }).then((res)=>{
@@ -11,10 +11,27 @@ function Todoform() {
     }).catch((err)=>{
         console.log(err)
     })
-    seTodo("")
+    setTodo("")
     }
   return (
-    <div>Todoform</div>
+    <div className="flex items-center justify-center space-x-4">
+      <label htmlFor="tuduInput" className="text-gray-800">
+        Add Tudu
+      </label>
+      <input
+        type="text"
+        id="tuduInput"
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
+        className="border border-gray-400 px-3 py-2 rounded"
+      />
+      <button
+        onClick={handleSubmit}
+        className="bg-violet-500 hover:bg-violet-600 text-white px-4 py-2 rounded"
+      >
+        Add
+      </button>
+    </div>
   )
 }
 
