b8f2d788a39d40e8ae7bd7546b35f57f8be8ac9f
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 16:22:58 +0530

    Styling Profile

--- todo-react/src/Profile.js
+++ todo-react/src/Profile.js
@@ -1,17 +1,43 @@
 import React,{useState,useEffect} from 'react'
 import { account } from './appwrite-config'
-
+import { useNavigate } from 'react-router-dom'
+import "./index.css"
 function Profile() {
     const [userdetails,setUserdetails] = useState()
     useEffect(()=>{
         account.get().then((res)=>{
             setUserdetails(res)
+            
         }).catch((err)=>{
             console.log("Error")
         }) 
     },[])
+
+    const navigate = useNavigate()
+    const handleLogout = async()=>{
+        try {
+            await account.deleteSession("current")
+            navigate("/")
+        } catch (error) {
+            
+        }
+    }
+    const handleDetails = ()=>{
+        console.log(userdetails)
+    }
   return (
-    <></>
+    <>
+        <div>
+            <div>
+                {userdetails ? (<div>Hello {userdetails.email} </div>) : (<div> Bye</div>)
+                 
+                }
+            </div>
+            <div>
+                <button onClick={handleLogout} className='text-sm bg-gray-400 border-2 border-black px-3 py-1 mt-4 rounded-sm ml-4 hover:bg-violet-500 hover:text-white'>Logout</button>
+            </div>
+        </div>
+    </>
   )
 }
 
