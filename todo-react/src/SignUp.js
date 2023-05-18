719328306336c2af53e6ce90c6c37b355455007f
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 10:32:06 +0530

    styling

--- todo-react/src/SignUp.js
+++ todo-react/src/SignUp.js
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './index.css'
 function SignUp() {
+    const [email,setEmail] = useState("")
+    const [password,setPassword]=useState("")
+    const handleEmailChange = ()=>{
+
+    }
+    const handlePasswordChange = ()=>{
+
+    }
+    const handleSubmit = ()=>{
+
+    }
   return (
     <>
-        <p className='text-bold-3x'>SignUp</p>
+        <div className="bg-cherry-red min-h-screen">
+      {/* Navbar */}
+      <div className="h-16 bg-off-white">
+        {/* Navbar content goes here */}
+      </div>
+
+      {/* Signup form */}
+      <div className="flex flex-col items-center justify-center pt-16">
+        <h1 className="text-lg text-black mb-8 sm:text-4xl">Signup</h1>
+
+        <form className="bg-off-white px-8 py-6 rounded shadow-md w-1/2 sm:w-2/3">
+          <div className="mb-4">
+            <label className="text-black text-lg">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              className="w-full bg-white border border-black px-3 py-2 rounded mt-2 "
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-black text-lg">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="w-full bg-white border border-black px-3 py-2 rounded mt-2"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="border-2 border-black py-2 px-4 rounded  mt-4 hover:bg-violet-700 hover:text-white" 
+          >
+            Create User
+          </button>
+        </form>
+      </div>
+    </div>
     </>
   )
 }
--- todo-react/tailwind.config.js
+++ todo-react/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,js}"],
+  content: [ "./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    screens:{
+      'sm': '320px',
+      'md': '640px',
+      'lg': '1024px',
+      'xl': '1280px'
+    }
   },
   plugins: [],
 }
