62e9debf1a460a57ca6619948c71140d6d29312e
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 16:26:26 +0530

    Added styles Profile

--- todo-react/src/Profile.js
+++ todo-react/src/Profile.js
@@ -29,12 +29,14 @@ function Profile() {
     <>
         <div>
             <div>
-                {userdetails ? (<div>Hello {userdetails.email} </div>) : (<div> Bye</div>)
+                {userdetails ? (<div className='ml-4 mt-32 sm:mt-6'>Hello {userdetails.email} </div>) : (<div> Bye</div>)
                  
                 }
             </div>
             <div>
-                <button onClick={handleLogout} className='text-sm bg-gray-400 border-2 border-black px-3 py-1 mt-4 rounded-sm ml-4 hover:bg-violet-500 hover:text-white'>Logout</button>
+                <button onClick={handleLogout} className='text-sm bg-gray-400 border-2 border-black px-3 py-1 mt-4 rounded-sm ml-4 hover:bg-violet-500 hover:text-white'>
+                Logout
+                </button>
             </div>
         </div>
     </>
