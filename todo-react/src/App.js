ad55edd6195521fe3a3155deafaa7f20edf744b9
Adithya V <adithyavinod1943@gmail.com>
2023-05-18 11:51:26 +0530

    initialized profile.js

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import SignUp from './SignUp';
 import Login from './Login';
+import Profile from './Profile';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" element={<h1>Home</h1>}/>
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
         </Routes>
       </BrowserRouter>
     </div>
