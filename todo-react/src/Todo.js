840f8934c8a55af3b077dafe6203ee7bfc7f0b6d
Adithya V <adithyavinod1943@gmail.com>
2023-05-20 23:52:31 +0530

    Finished the Complete button Funtion, updates the record

--- todo-react/src/Todo.js
+++ todo-react/src/Todo.js
@@ -15,21 +15,24 @@ function Todo() {
         await databases.deleteDocument("6465a1e5c4ed1e82b0f5","6465a1f120228e9beaf7",id)
         window.location.reload()
     }
+    const handleComplete = async(id,data)=>{
+      await databases.updateDocument("6465a1e5c4ed1e82b0f5","6465a1f120228e9beaf7",id,{
+        todo:`${data} (Completed)`
+      })
+      window.location.reload()
+    }
   return (
     <> {
         todos && todos.map((item)=>{
             return(
-                <div key={item.$id} className="bg-white rounded shadow-md p-4 mx-auto max-w-xs sm:max-w-sm md:max-w-lg mb-2">
+                <div key={item.$id}  className="bg-white rounded shadow-md p-4 mx-auto max-w-xs sm:max-w-sm md:max-w-lg mb-2">
       <div className="flex items-center justify-between">
         <p  className="text-gray-800">{item.todo}</p>
         
       </div>
 
       <div className="flex items-center justify-between mt-2">
-      <div>
-        <input type="checkbox" className="form-checkbox" />
-        <label className="ml-2 text-gray-700">Complete</label>
-        </div>
+              <button className='text-green-500 focus:outline-none' onClick={()=>handleComplete(item.$id,item.todo)}>Complete</button>
         <button className="text-red-500 focus:outline-none" onClick={()=>handleDelete(item.$id)}>
         Delete
         </button>
