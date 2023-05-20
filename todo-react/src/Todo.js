9a76e72a12c04d84eb20fa4bd06f0ff310120a2b
Adithya V <adithyavinod1943@gmail.com>
2023-05-20 22:28:22 +0530

    Added listdocuments

--- todo-react/src/Todo.js
+++ todo-react/src/Todo.js
@@ -1,8 +1,17 @@
 import React,{useState,useEffect} from 'react'
-
+import { account } from './appwrite-config'
+import { databases } from './appwrite-config'
 function Todo() {
+    const [todos,setTodos] = useState()
+    useEffect(()=>{
+        databases.listDocuments("6465a1f120228e9beaf7").then((res)=>{
+            setTodos(res.documents)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    },[])
   return (
-    <div>Todo</div>
+    <></>
   )
 }
 
